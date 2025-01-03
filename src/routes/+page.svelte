3bcc9f884ec2aeb44da81846c529398a7a357829
SL6I <s443004498@st.uqu.edu.sa>
2025-01-03 21:19:28 +0300

    Shadow

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,25 +2,30 @@
     <title>Calc</title>
 </svelte:head>
 
-<div class="bg-zinc-400 h-fit w-fit rounded-3xl grid grid-cols-4 gap-3">
-    <button> % </button>
-    <button> √ </button>
-    <button> CE </button>
-    <button> C </button>
+<div class="bg-slate-50 h-fit w-fit rounded-3xl grid grid-cols-4 gap-3 py-6 px-4 font-semibold
+    shadow-2xl">
+    <div class="bg-blue-600 rounded-full col-span-4 h-12 flex
+     items-center px-4 mb-2 text-white text-xlg font-semibold">
+        123456
+    </div>
+    <button class="bg-[#f3f6fc]"> % </button>
+    <button class="bg-[#f3f6fc]"> √ </button>
+    <button class="bg-[#f3f6fc]"> CE </button>
+    <button class="bg-[#1f2b54] text-white"> C </button>
     <button> 7 </button>
     <button> 8 </button>
     <button> 9 </button>
-    <button> - </button>
+    <button class="bg-[#fd3f59] text-white"> - </button>
     <button> 4 </button>
     <button> 5 </button>
     <button> 6 </button>
-    <button> ÷ </button>
+    <button class="bg-[#2285fd] text-white"> ÷ </button>
     <button> 1 </button>
     <button> 2 </button>
     <button> 3 </button>
-    <button> x </button>
+    <button class="bg-[#f9c80e] text-white"> x </button>
     <button> . </button>
     <button> 0 </button>
     <button> = </button>
-    <button> + </button>
+    <button class="bg-[#63dc74] text-white"> + </button>
 </div>
